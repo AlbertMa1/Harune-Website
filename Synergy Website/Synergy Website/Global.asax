155ae06f5a1d78aaad6d383cfa1d0e779351416e
CuddlesthePanda <maalber1@msu.edu>
2018-07-19 23:03:34 -0400

    First Commit

---
+++ Synergy Website/Synergy Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Synergy_Website.Global" Language="C#" %>
---
+++ Synergy Website/Synergy Website/Site.Master.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.Adapters;
+
+namespace Synergy_Website
+{
+    public partial class SiteMaster : MasterPage
+    {
+
+    }
+    public class ScriptManagerProxy : Control
+    {
+
+    }
+}
